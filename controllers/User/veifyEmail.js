028ffce96a5a44636a9f5d7a0c1c0953ea71e905
saman <sam.karimi727@gmail.com>
2025-05-15 23:15:45 +0330

    update veifyEmail.js

--- controllers/User/veifyEmail.js
+++ controllers/User/veifyEmail.js
@@ -1,44 +1,38 @@
-import UserModel from "../../models/User.js"
-
-
+import UserModel from '../../models/User.js'
 
 const verifyEmailController = async (req, res) => {
-    try {
-      const { id } = req.body
-      const payload = {
+  try {
+    const { id } = req.body
 
+    const user = await UserModel.findByIdAndUpdate(
+      id,
+      {
         verify_email: true,
-      }
+      },
+      { new: true }
+    )
 
-
-      const user = await UserModel.findByIdAndUpdate(id , payload , { new : true})
-  
-  
-  
-      if (!user) {
-        return res.status(400).json({
-          message: 'invalid code',
-          error: true,
-          success: false,
-        })
-      } 
-  
-
-  
-      return res.status(200).json({
-        data : user ,
-        message: 'verify Email Done',
-        error: false,
-        success: true,
-      })
-    } catch (error) {
-      return res.status(500).json({
-        success: false,
+    if (!user) {
+      return res.status(400).json({
+        message: 'invalid code',
         error: true,
-        message: 'Server Error 😡😡😡',
+        success: false,
       })
     }
+
+    return res.status(200).json({
+      data: user,
+      message: 'verify Email Done',
+      error: false,
+      success: true,
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: true,
+      message: 'Server Error 😡😡😡',
+    })
   }
+}
 
-
-  export default verifyEmailController
+export default verifyEmailController
