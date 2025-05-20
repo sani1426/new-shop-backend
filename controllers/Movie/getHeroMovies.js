8c01f15009dfd6adcf35a11107de8b96c5a73fcb
saman <sam.karimi727@gmail.com>
2025-05-21 02:08:18 +0330

    create 1 file and update 1 file

---
+++ controllers/Movie/getHeroMovies.js
@@ -0,0 +1,24 @@
+import MovieModel from '../../models/Movie.js'
+
+const getHeroMoviesController = async (req, res) => {
+  try {
+    const movies = await MovieModel.find()
+      .sort({ rating: -1 })
+      .limit(5)
+
+    return res.status(200).json({
+      success: true,
+      error: false,
+      message: 'Successfully',
+      data: movies,
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: true,
+      message: 'Server Error 😡😡😡',
+    })
+  }
+}
+
+export default getHeroMoviesController
--- routes/MoviesRouters.js
+++ routes/MoviesRouters.js
@@ -2,11 +2,13 @@
 import express from 'express'
 import getAllMoviesController from '../controllers/Movie/GetAllMovies.js'
 import getMoviesByGenreController from '../controllers/Movie/getMoviesByGenre.js'
+import getHeroMoviesController from '../controllers/Movie/getHeroMovies.js'
 
 const router = express.Router()
 
 router.get('/' , getAllMoviesController)
 router.get('/genres' , getMoviesByGenreController)
+router.get('/hero-movies' , getHeroMoviesController)
 
 
 export default router
